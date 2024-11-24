28a609683e402eecefdf229d0e0dbdf0cdf1c1ad
brianaqp <brianqp9889@gmail.com>
2024-11-24 13:00:03 +0900

    feat: googleClient

--- prisma/seeds/002_places.ts
+++ prisma/seeds/002_places.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-import GoogleApi from "prisma/utils/googleApi";
+import {Prisma, PrismaClient} from "@prisma/client";
+import GoogleClient from "../utils/googleClient";
 
 const apiKey = process.env.GOOGLE_API_KEY;
-const googleApi = new GoogleApi(apiKey);
+const googleApi = new GoogleClient(apiKey);
 const prisma = new PrismaClient();
 
 async function main() {
@@ -10,10 +10,14 @@ async function main() {
 
   const result = await googleApi.textSearch(query);
 
-  for (let place in result.places) {
+  if (!result) {
+    return null
+  }
+
+  for (let place of result) {
     // Validation per place
     const existingPlace = await prisma.places.findUnique({
-      where: { google_place_id: place.googlePlaceId },
+      where: { google_place_id: place.id },
     });
 
     // If this element exist, continue in the next iteration
@@ -21,29 +25,35 @@ async function main() {
       continue;
     }
 
+    // [ ] We need validation!! we don't want data that is not complete in the most important fields.
+
     // Insert the existing place
     const newPlace = await prisma.places.create({
       data: {
-        google_place_id: place.googlePlaceId,
+        google_place_id: place.id,
         formatted_address: place.formattedAddress,
-        address: result.address,
-        latitude: new Prisma.Decimal(place.location.lat),
-        longitude: new Prisma.Decimal(place.locatin.log),
+        address: place.formattedAddress || "null", // Its the same?
+        // Add validation to fields, we don't want this.
+        latitude: new Prisma.Decimal(place.location?.latitude),
+        longitude: new Prisma.Decimal(place.location?.longitude),
         google_map_uri: place.googleMapsUri,
         website_uri: place.websiteUri,
-        name: place.name,
-        general_info: result.generalInfo || "General info not provided",
+        name: place.displayName  || "null",
+        general_info: "General info not provided", // Google maps provide general info?
       },
     });
-  }
 
-  // 2nd Part. Working with photos
-  const photos = result.place.photos;
-  const MAX_PHOTOS = 5;
-  if (photos && Array.isArray(photos)) {
-    for (const photo of photos.slice(0, MAX_PHOTOS)) {
-      // We don't know how the response looks like yet.
-      const image = await googleApi.photoByPlace(photo);
+    // Who we know if this was created sucesfully?
+    // [ ] Add a console log to see how many records with save.
+
+    // 2nd Part. Working with photos
+    let photos = place.photos;
+    const MAX_PHOTOS = 1;
+    if (photos && Array.isArray(photos)) {
+      for (const photo of photos.slice(0, MAX_PHOTOS)) {
+        const base64_encoded = await googleApi.photoByPlace(photo.name);
+        // [ ] Insert the base64 result in their corresponding table.
+      }
     }
   }
 }
