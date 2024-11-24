1b95297c82fbc8b9944b37ee1eb9973150d5decd
ai-n0808 <aichan.88.aichan@gmail.com>
2024-11-24 16:02:52 +0900

    feat: add endpoints

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,119 @@
 import express, { Request, Response, Router } from "express";
-import { PrismaClient, Prisma } from "@prisma/client";
-import GeneralModel from "src/models";
+import { PrismaClient } from "@prisma/client";
+// import GeneralModel from "src/models";
 
-// Router
+const prisma = new PrismaClient();
 const router: Router = express.Router();
 
-router.get("/places", async (req: Request, res: Response) => {
-  const data = GeneralModel.getInitialData();
+// --! Places andpoints
 
-  res.send({
-    data,
-  });
+//Retrieve the Place interfaced object
+router.get("/places", async (req: Request, res: Response) => {
+  try {
+    const places = await prisma.places.findMany({
+      include: {
+        experiences: true,
+        images: true,
+      },
+    });
+    res.json(places);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
+
+//Add a new place
+router.post("/places", async (req: Request, res: Response) => {
+  try {
+    const placeData = req.body;
+    const newPlace = await prisma.places.create({
+      data: placeData,
+    });
+    res.status(201).json(newPlace);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+//Update details of specific place
+router.patch("/places/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+    const updatedPlace = await prisma.places.update({
+      where: { id: Number(id) },
+      data: updateData,
+    });
+    res.json({ updatedPlace });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+//Remove place by its ID
+router.delete("/places/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await prisma.places.delete({
+      where: { id: Number(id) },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+// --! Experiences endpoints
+
+//Retrieve all experiences related to a specific place
+router.post("/places/:id/experiences", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const experienceData = req.body;
+    const newExperience = await prisma.experiences.create({
+      data: {
+        ...experienceData,
+        place_id: Number(id),
+      },
+    });
+    res.status(201).json(newExperience);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+//Update details of a specific experience
+router.patch(
+  "/places/:id/experiences/:expId",
+  async (req: Request, res: Response) => {
+    try {
+      const expID = req.params;
+      const updateData = req.body;
+      const updatedExperience = await prisma.experiences.update({
+        where: { id: Number(expID) },
+        data: updateData,
+      });
+      res.json(updatedExperience);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+);
+
+//Remove experience by its ID
+router.delete(
+  "/places/:id/experiences/:expId",
+  async (req: Request, res: Response) => {
+    try {
+      const expID = req.params;
+      await prisma.experiences.delete({
+        where: { id: Number(expID) },
+      });
+      res.status(204).send();
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+);
 
 export default router;
