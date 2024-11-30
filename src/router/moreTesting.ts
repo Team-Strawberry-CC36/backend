12bcd330255728363c59a71fb41dcb25d5b0e13a
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-30 23:15:46 +0900

    chore: request endpoints for posting and patching etiquette votes

--- src/router/moreTesting.ts
+++ src/router/moreTesting.ts
@@ -2,20 +2,12 @@ import { Request, Response, Router } from 'express';
 
 const moreTestingRouter: Router = Router();
 
-moreTestingRouter.post('/places/:id/votes', (req: Request, res: Response) => {
-    const { votes, userId } = req.body;
-    // Process the votes into a table of some sort
-    console.log(votes);
-    console.log(userId);
-    res.status(200).json({"message": "success"});
-});
-
 moreTestingRouter.get('/places/:id/votes', (req: Request, res: Response) => {
     // Please get data prepared in the following example format
-    // Interface in frontend/src/utils/interfaces/PlaceEtiquetteVotes.ts
+    // Interface for data format is in frontend/src/utils/interfaces/PlaceEtiquetteVotes.ts
     
-    // Feel free to change this variable name
-    // User id can be retrieved from firebase authentication.
+    // Feel free to change variable name from mockEtiquetteVotesData to something else
+    // userID property can be retrieved from firebase authentication
     const mockEtiquetteVotesData = {
         placeId: 1,
         userId: 'users uid from firebase',
@@ -38,6 +30,65 @@ moreTestingRouter.get('/places/:id/votes', (req: Request, res: Response) => {
 
     // Please make sure that data is returned like this
     res.status(200).json({ message: "Hooray, it was a success", data:mockEtiquetteVotesData });
+});
+
+moreTestingRouter.post('/places/:id/votes', (req: Request, res: Response) => {
+    const { votes, placeId } = req.body;
+    console.log(req.body);
+
+    // The data sent to this endpoint looks like this:
+    // {
+    //     votes: [
+    //       { etiquetteId: 1, etiquetteType: 'Smoking' },
+    //       { etiquetteId: 2, etiquetteType: 'Tattoos', vote: 'allowed' },
+    //       { etiquetteId: 3, etiquetteType: 'Towels', vote: 'not-allowed' },
+    //       { etiquetteId: 4, etiquetteType: 'Swimming' },
+    //       { etiquetteId: 5, etiquetteType: 'Existential Dread', vote: 'allowed' }
+    //     ],
+    //     placeId: 1
+    // }
+
+    // If there is no "vote" property on an object in the votes array, it is because the user did not select to vote for it
+    // so on the front end, it is undefined
+    // Interface for front end can be found at frontend/src/utils/interfaces/PlaceEtiquetteVotes.ts
+    // But for reference, each object in the votes array follows these:
+    // interface IEtiquetteUsersVote {
+    //     etiquetteId: number;
+    //     etiquetteType: string;
+    //     vote: EtiquetteStatus;
+    // }
+    // type EtiquetteStatus = 'allowed' | 'not-allowed' | undefined;
+
+    res.status(200).json({ "message": "success" });
+});
+
+moreTestingRouter.patch('/places/:id/votes', (req: Request, res: Response) => {
+    const { votes, placeId } = req.body;
+    console.log(req.body);
+
+    // The data sent to this endpoint looks like this:
+    // {
+    //     votes: [
+    //       { etiquetteId: 1, etiquetteType: 'Smoking' },
+    //       { etiquetteId: 2, etiquetteType: 'Tattoos', vote: 'allowed' },
+    //       { etiquetteId: 3, etiquetteType: 'Towels', vote: 'not-allowed' },
+    //       { etiquetteId: 4, etiquetteType: 'Swimming' },
+    //       { etiquetteId: 5, etiquetteType: 'Existential Dread', vote: 'allowed' }
+    //     ],
+    //     placeId: 1
+    // }
+
+    // If there is no "vote" property on an object in the votes array, it is because the user did not select to vote for it
+    // so on the front end, it is undefined
+    // Interface for front end can be found at frontend/src/utils/interfaces/PlaceEtiquetteVotes.ts
+    // But for reference, each object in the votes array follows these:
+    // interface IEtiquetteUsersVote {
+    //     etiquetteId: number;
+    //     etiquetteType: string;
+    //     vote: EtiquetteStatus;
+    // }
+    // type EtiquetteStatus = 'allowed' | 'not-allowed' | undefined;
+    res.status(200).json({ "message": "success" });
 })
 
 export default moreTestingRouter;
