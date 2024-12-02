19f36a716e3d25f2e94c2c6c3fce01181edae0be
ai-n0808 <aichan.88.aichan@gmail.com>
2024-12-02 17:51:35 +0900

    fix: add interface

--- src/interfaces/places.ts
+++ src/interfaces/places.ts
@@ -87,6 +87,32 @@ interface ApiResponseEtiquetteVotes<T> {
   data: T;
 }
 
+interface IEtiquetteVotes {
+  etiquetteId: number;
+  etiquetteType: string;
+  numberOfVotesForAllowed: number;
+  numberOfVotesForNotAllowed: number;
+}
+
+interface IEtiquetteUsersVote {
+  etiquetteId: number;
+  etiquetteType: string;
+  vote: EtiquetteStatus;
+}
+
+interface IPlaceEtiquetteVotesAlias {
+  placeId: number;
+  userId: string | undefined;
+  userHasVoted: boolean;
+  etiquetteVotes: IEtiquetteVotes[];
+  usersVote: IEtiquetteUsersVote[];
+}
+
+interface ApiResponseEtiquetteVotes<T> {
+  message: string;
+  data: T;
+}
+
 export type IPlaceEtiquetteVotes =
   ApiResponseEtiquetteVotes<IPlaceEtiquetteVotesAlias>;
 
