fc83ad3c10ff908b982bfbbe512a7806d4fa7f76
kiw1meister <linandy719@gmail.com>
2024-12-03 18:09:47 +0900

    chore: English fix

--- prisma/seeds/003_etiquette.ts
+++ prisma/seeds/003_etiquette.ts
@@ -6,28 +6,28 @@ async function etiquettes() {
   await prisma.etiquette.deleteMany();
   await prisma.etiquette.createMany({
     data: [
-      { place_type: "ONSEN", label: "tattoos" },
-      { place_type: "ONSEN", label: "Reservation" },
-      { place_type: "ONSEN", label: "Book time slot" },
-      { place_type: "ONSEN", label: "Provided towel" },
-      { place_type: "ONSEN", label: "Swimwear and swim hat" },
+      { place_type: "ONSEN", label: "Tattoos" },
+      { place_type: "ONSEN", label: "Reservations" },
+      { place_type: "ONSEN", label: "Time slot booking" },
+      { place_type: "ONSEN", label: "Towels provided" },
+      { place_type: "ONSEN", label: "Swimwear" },
       { place_type: "ONSEN", label: "Tie up long hair" },
-      { place_type: "ONSEN", label: "Bring children into the bath" },
+      { place_type: "ONSEN", label: "Children" },
 
-      { place_type: "SHRINE", label: "Take photos everywhere" },
-      { place_type: "SHRINE", label: "Throw coins into the offering box" },
+      { place_type: "SHRINE", label: "Taking photos" },
+      { place_type: "SHRINE", label: "Coin offerings" },
       { place_type: "SHRINE", label: "Clap your hands during prayer" },
       {
         place_type: "SHRINE",
-        label: "Touch sacred objects or ropes (shimenawa)",
+        label: "Touching sacred objects or ropes (shimenawa)",
       },
-      { place_type: "SHRINE", label: "Eating and drinking" },
+      { place_type: "SHRINE", label: "Food and drinks" },
 
       { place_type: "RESTAURANT", label: "Reservation" },
-      { place_type: "RESTAURANT", label: "Take off the shoes" },
-      { place_type: "RESTAURANT", label: "smokings" },
-      { place_type: "RESTAURANT", label: "Bringing children" },
-      { place_type: "RESTAURANT", label: "Bringing pets" },
+      { place_type: "RESTAURANT", label: "Shoes" },
+      { place_type: "RESTAURANT", label: "Smoking" },
+      { place_type: "RESTAURANT", label: "Children" },
+      { place_type: "RESTAURANT", label: "Pets" },
     ],
   });
 
