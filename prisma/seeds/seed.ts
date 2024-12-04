a75603c119ad1b657bd3c8a48c190aa33c680bea
brianaqp <brianqp9889@gmail.com>
2024-12-04 18:18:57 +0900

    hotfix: prisma seeding

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -1,9 +1,41 @@
 import { PrismaClient } from "@prisma/client";
-import etiquettes from "./003_etiquette";
 
-async function main() {
+const prisma = new PrismaClient();
+
+async function setDBToInitialState() {
   await etiquettes();
-  console.log("Seeding apply");
 }
 
-main();
+setDBToInitialState();
+
+async function etiquettes() {
+  // Reset Autoincrement
+  await prisma.etiquette.createMany({
+    data: [
+      { place_type: "ONSEN", label: "Tattoos" },
+      { place_type: "ONSEN", label: "Reservations" },
+      { place_type: "ONSEN", label: "Time slot booking" },
+      { place_type: "ONSEN", label: "Towels provided" },
+      { place_type: "ONSEN", label: "Swimwear" },
+      { place_type: "ONSEN", label: "Tie up long hair" },
+      { place_type: "ONSEN", label: "Children" },
+
+      { place_type: "SHRINE", label: "Taking photos" },
+      { place_type: "SHRINE", label: "Coin offerings" },
+      { place_type: "SHRINE", label: "Clap your hands during prayer" },
+      {
+        place_type: "SHRINE",
+        label: "Touching sacred objects or ropes (shimenawa)",
+      },
+      { place_type: "SHRINE", label: "Food and drinks" },
+
+      { place_type: "RESTAURANT", label: "Reservation" },
+      { place_type: "RESTAURANT", label: "Shoes" },
+      { place_type: "RESTAURANT", label: "Smoking" },
+      { place_type: "RESTAURANT", label: "Children" },
+      { place_type: "RESTAURANT", label: "Pets" },
+    ],
+  });
+}
+
+export default etiquettes;
